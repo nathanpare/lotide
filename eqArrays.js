dbb7d7d90e5c2ffcafc0d1859c2682292f02c020
Nathan Pare <nathanparebusiness@gmail.com>
2022-01-31 21:10:09 -0800

    Letting assertEqual work with eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,11 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = (a,b) =>
-a.length === b.length && a.every((v, i) => v === b[i]);
-
 const a = [];
 const b = [];
+const Str = 'a';
+const strObj = 'a';
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-
+console.log(assertEqual(eqArrays(a, b), true));
+console.log(assertEqual(eqArrays([Str], [strObj]), true));
+console.log(assertEqual(eqArrays([null], [undefined]), false));
