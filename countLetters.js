ce84289c3325e81b5bc98d1ff7316b2cc0213e07
Nathan Pare <nathanparebusiness@gmail.com>
2022-02-02 15:16:51 -0800

    output an object listing the amount of each letter in a word

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return (`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  return string.split(' ').reduce(function(a, b) {
+    b.split('').reduce(function(c, d) {
+      return (c[d] = (c[d] || 0) + 1 , c);
+    }, a[b] = {});
+
+    return a;
+  }, {});
+}
+
+console.log(countLetters("lighthouse"));
