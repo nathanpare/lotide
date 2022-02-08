3de7cbf4376051de0d402643a4d72de22a127bd1
Nathan Pare <nathanparebusiness@gmail.com>
2022-02-07 16:03:02 -0800

    creating a map function

---
+++ map.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+
+const results2 = map(words, word => word[word.length - 1]);
+
+assertArraysEqual(results2, [ 'd', 'l', 'o', 'r', 'm' ]);
+
