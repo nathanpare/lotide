127cea66fd60b9591bce828219d17925d1a51ae3
Nathan Pare <nathanparebusiness@gmail.com>
2022-02-16 14:10:37 -0800

    Refactor AssertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,11 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     return (`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
     return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
   }
-};
-
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false;
-    }
-  }
-  return true;
 };
 
 module.exports = assertArraysEqual;
